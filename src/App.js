0a80365f2db3729d3be0c134e5e96c2249177ae5
shaipranesh2 <f20200731@pilani.bits-pilani.ac.in>
2023-05-05 22:56:48 +0300

    deployed website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Main from './Main';
 import './App.css';
 
 import Welcome from './Welcome';
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Welcome/>
+      <Main/>
 
     </div>
   );
