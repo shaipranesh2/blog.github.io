4238dd12328847c5939c47217a69ba4e1b31a180
Shaipranesh S <80086120+shaipranesh2@users.noreply.github.com>
2023-05-07 22:44:01 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ export default function Main(){
         <div className="Name">
             <h1 className="title-blogs">GSoC 2023- Red hen lab</h1>
             <h3 className="subtitle">Project details</h3>
-            <p>Hello everyone, I will spending this summer contributing to Red Hen labs under the Google Summer of Code 2023 program.
+            <p>Hello everyone, I will be spending this summer contributing to Red Hen labs under the Google Summer of Code 2023 program.
 This project aims to improve on the previous year work on "Classification of body-keypoint trajectories
 of gesture co-occurring with time expressions" by proposing a Graph neural networks to better exploit
 the dataset’s nature and using fusion techniques to combine the keypose and audio data to tackle the
